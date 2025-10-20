1c637a1c8d1855a88653b3d6ecac186969f9f397
Fabian15021502 <fabianalexandervanegasbeltran@gmail.com>
2025-10-20 11:55:09 -0500

    requisito para crear pip stop

--- app/src/main/java/data/repository/IPitStopRepository.kt
+++ app/src/main/java/data/repository/IPitStopRepository.kt
@@ -15,3 +15,16 @@ interface IPitStopRepository {
      * Retorna un flujo de Pit Stops filtrados por un término de búsqueda (piloto o escudería).
      */
     fun searchPitStops(query: String): Flow<List<PitStop>>
+
+    // Requisito: Guardar pit stop (se usa para insertar y actualizar)
+    /**
+     * Guarda un nuevo PitStop o actualiza uno existente.
+     */
+    suspend fun savePitStop(pitStop: PitStop)
+
+    // Requisito: Eliminar pit stop
+    /**
+     * Elimina un PitStop de la base de datos por su ID.
+     */
+    suspend fun deletePitStop(id: Int)
+}
